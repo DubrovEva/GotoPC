ade464316cecf23c6123c3594d4f39764e75c171
DubrovEva <42350479+DubrovEva@users.noreply.github.com>
2018-08-19 15:44:39 +0300

    Update WebExam1.scala

--- WebExam1.scala
+++ WebExam1.scala
@@ -1,22 +1,25 @@
 import scala.util._
 import scala.concurrent._
 import scala.concurrent.duration._
-
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App{
 
   def safeList(list: List[Future[Try[Option[Int]]]]) : List[Int] = {
     if (list.length > 1) {
-      val elem Await.result(list.head, Duration.Inf) match {
-        case Success(succ) => succ
-        case Failure(ex) => "ERROR"}
-      List(elem.getOrElse("ERROR")*10):::safeList(list.tail)
+      val elem: Int = Await.result(list.head, Duration.Inf) match {
+        case Success(succ) => succ.get
+        case Failure(ex) => throw new Exception}
+    List(elem*10):::safeList(list.tail)
       }
     else {
-      val elem Await.result(list.head, Duration.Inf) match {
-        case Success(succ) => succ
-        case Failure(ex) => "ERROR"}
-      List(elem.getOrElse("ERROR")*10)
+      val elem = Await.result(list.head, Duration.Inf) match {
+        case Success(succ) => succ.get
+        case Failure(ex) => throw new Exception}
+    List(elem*10)
     }
   }
+
+  val list : List[Future[Try[Option[Int]]]] = List(Future{Try(Option(1))}, Future{Try(Option(10))})
+  println(safeList(list))
 }
