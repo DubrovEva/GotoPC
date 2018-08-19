c0c00e42a3aba72e21bd9ede390374a595c6012d
DubrovEva <42350479+DubrovEva@users.noreply.github.com>
2018-08-19 13:45:47 +0300

    Create WebExam2.scala

---
+++ WebExam2.scala
@@ -0,0 +1,19 @@
+object Main extends App{
+
+  def safeList(list: List[Try[Option[Any]]]) : Int = {
+    if (list.length > 1) {
+      val elem = list.head match {
+        case Success(succ) => succ
+        case Failure(ex) => None}
+      if (elem.getOrELse(None) == None) 1 + safeList(list.tail)
+      else 0+safeList(list.tail)
+      }
+    else {
+      val elem = list.head match {
+        case Success(succ) => succ
+        case Failure(ex) => None}
+      if (elem == 0) 1
+      else 0
+    }
+  }
+}
