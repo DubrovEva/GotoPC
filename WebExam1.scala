872da7ce935f848314405219df953a18ad454ced
DubrovEva <42350479+DubrovEva@users.noreply.github.com>
2018-08-19 13:54:45 +0300

    Update WebExam1.scala

--- WebExam1.scala
+++ WebExam1.scala
@@ -4,14 +4,19 @@ import scala.concurrent.duration._
 
 
 object Main extends App{
-  def newList(list: List[Int]): List[Int] = {
-    if (list.length > 1) List(list.head*10):::newList(list.tail)
-    else List(list.head*10)
-  }
 
-  def safeList(list: List[Future[Try[Option[Int]]]]) : Any =
-    newList(Await.result(list.getOrElse("ERROR"), Duration.Inf)) match {
-  case Success(succ) => succ
-  case Failure(ex) => throw new Exception
+  def safeList(list: List[Future[Try[Option[Int]]]]) : List[Int] = {
+    if (list.length > 1) {
+      val elem Await.result(list.head, Duration.Inf) match {
+        case Success(succ) => succ
+        case Failure(ex) => "ERROR"}
+      List(elem.getOrElse("ERROR")*10):::safeList(list.tail)
+      }
+    else {
+      val elem Await.result(list.head, Duration.Inf) match {
+        case Success(succ) => succ
+        case Failure(ex) => "ERROR"}
+      List(elem.getOrElse("ERROR")*10)
+    }
   }
 }
