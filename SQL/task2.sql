30aa2c8141f92bd1d587a203e13351d8127b6155
DubrovEva <42350479+DubrovEva@users.noreply.github.com>
2018-08-22 18:40:40 +0300

    Update task2.sql

--- SQL/task2.sql
+++ SQL/task2.sql
@@ -1,11 +1,3 @@
-create table table1 (id INT, name VARCHAR(100), age INT, salary INT, PRIMARY KEY(id));
-create table table2 (id INT, phone_number INT, years_of_experience INT);
-
-INSERT INTO table1 VALUES (1, 'John', 23, 400);
-INSERT INTO table1 VALUES(2, 'Mary', 20, 450);
-INSERT INTO table1 VALUES(3, 'Max', 29, 1200);
-INSERT INTO table1 VALUES (4, 'Felix', 18, 350);
-INSERT INTO table1 VALUES(5, 'Groover', 22, 570);
 INSERT INTO table1 VALUES(6, 'Ben', 19, 620);
 
 INSERT INTO table2 VALUES(1, 433443, 2), (2, 111111, 1), (3, 123456, 7), (4, 123568, 0), (5, 191919, 1), (6, 121212, 1);
@@ -15,3 +7,16 @@ Exists (select * from table1
        where table1.id = table2.id AND name='Max')
 
 select * from table2
+select * from table1 WHERE age >= '23' AND age < '27' AND salary='1000';
+select * from table1 WHERE name = 'Mary' OR name = 'John';
+select phone_number from table2 WHERE
+Exists (select * from table1 
+       where table1.id = table2.id AND name='John')
+
+select * from table1 WHERE
+Exists (select * from table2 
+       where table1.id = table2.id AND phone_number/100000='1')
+
+select * from table1 WHERE
+Exists (select * from table2 
+       where table1.id = table2.id AND years_of_experience>'2')
